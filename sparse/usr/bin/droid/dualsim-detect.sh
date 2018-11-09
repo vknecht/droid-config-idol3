c4fa9ec4d8bd363da02860fcb782332528b16434
Vincent Knecht <vincent.knecht@mailoo.org>
2018-11-09 14:01:45 +0100

    [dhc] Added run-time detection of dualsim 6045K variant
    Change-Id: Iac8e4488662fd8d6695719facd5bba19de43da8f

---
+++ sparse/usr/bin/droid/dualsim-detect.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ -f /.dualsim-detect-done ] ; then
+	exit 0
+fi
+
+model=$(getprop ro.cm.curef)
+
+if [ "X$model" = "X6045K" ] ; then
+	cp /etc/ofono/ril_subscription-6045K.conf \
+	/etc/ofono/ril_subscription.conf
+
+	cp /usr/share/csd/settings.d/hw-settings-6045K.ini \
+	/usr/share/csd/settings.d/hw-settings.ini
+
+	echo $model > /.dualsim-detect-done
+fi
+
