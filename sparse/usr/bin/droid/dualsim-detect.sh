61c81a28377ea1e6107c0d24e7f6487643fd5931
Vincent Knecht <vincent.knecht@mailoo.org>
2019-03-21 17:57:32 +0100

    [csd] Rename hw-settings-6045K.ini so it's no loaded by CSD in addition to hw-settings.ini
    Change-Id: Ic5e6f72525d2ca150dd76301446f21c621e250ea

--- sparse/usr/bin/droid/dualsim-detect.sh
+++ sparse/usr/bin/droid/dualsim-detect.sh
@@ -10,7 +10,7 @@ if [ "X$model" = "X6045K" ] ; then
 	cp /etc/ofono/ril_subscription-6045K.conf \
 	/etc/ofono/ril_subscription.conf
 
-	cp /usr/share/csd/settings.d/hw-settings-6045K.ini \
+	cp /usr/share/csd/settings.d/hw-settings-6045K.ini.template \
 	/usr/share/csd/settings.d/hw-settings.ini
 
 	echo $model > /.dualsim-detect-done
